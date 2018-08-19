50a8323fcae74b9d2570c5e542fdba0e65bac564
Doug Beney <work@dougie.io>
2018-08-18 22:59:42 -0400

    Big changes

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<v-app id="inspire">
+	<Sidebar></Sidebar>
+	<Toolbar></Toolbar>
+	<v-content>
+		<v-container fluid>
+
+		</v-container>
+	</v-content>
+</v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Sidebar from '@/components/Sidebar.vue'
+import Toolbar from '@/components/Toolbar.vue'
+export default {
+	components: {
+		Sidebar,
+		Toolbar,
+	}
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+</script>
+
+<style lang="sass">
+#keep main .container
+	height: 660px
+.v-navigation-drawer__border
+	display: none
+.text
+	font-weight: 400
 </style>
