aa1c21cb8eb9c1b0ce1d77ac0fb6514668d42c81
Doug Beney <work@dougie.io>
2018-08-21 09:34:04 -0400

    simplemde

--- src/components/parts/SimpleMDE.vue
+++ src/components/parts/SimpleMDE.vue
@@ -1,27 +1,35 @@
 <template>
-  <textarea ref="area"></textarea>
+	<textarea ref="area"></textarea>
 </template>
 
 <script>
 import simplemde from 'simplemde'
 
 export default {
-  props: ['value'],
-  mounted() {
-    this.mde = new simplemde({element: this.$refs.area })
-    this.mde.value(this.value)
-    var self = this
-    this.mde.codemirror.on('change', function() {
-      self.$emit('input', self.mde.value())
-    })
-  },
-  watch: {
-    // this would update on every keystroke, so maybe you have to remove it.
-    // component should work nonetheless, but if an external source changed the value, it would not reflect in this component.
-    value(newVal) { this.mde.value(newVal) } 
-  },
-  beforeDestroy() {
-    this.mde.toTextArea() // clean up when component gets destroyed.
-  }
+	props: ['value'],
+	mounted() {
+		this.mde = new simplemde({
+			element: this.$refs.area,
+			toolbar: false,
+			autosave: {
+				enabled: false
+			},
+			spellChecker: false,
+			status: false
+		})
+		this.mde.value(this.value)
+		var self = this
+		this.mde.codemirror.on('change', function() {
+			self.$emit('input', self.mde.value())
+		})
+	},
+	watch: {
+		// this would update on every keystroke, so maybe you have to remove it.
+		// component should work nonetheless, but if an external source changed the value, it would not reflect in this component.
+		value(newVal) { this.mde.value(newVal) } 
+	},
+	beforeDestroy() {
+		this.mde.toTextArea() // clean up when component gets destroyed.
+	}
 }
 </script>
